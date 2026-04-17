a2c0671f3a8b44d594265a0b75394f21d80f994b
Aleksei-Uzin <aleksei.uzin@gmail.com>
2026-04-17 16:32:11 +0300

    chore: set up project tooling: - add Prettier and ESLint with Airbnb base config - add Husky pre-commit hook and EditorConfig file

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import airbnb from "eslint-config-airbnb-base";
+import prettier from "eslint-config-prettier";
+import { defineConfig } from "@eslint/config-helpers";
+
+export default defineConfig([
+  airbnb,
+  prettier,
+]);
