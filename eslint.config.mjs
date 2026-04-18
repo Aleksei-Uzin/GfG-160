8f092c90b103527a6da079d7bc7093d9c8a8b805
Aleksei-Uzin <aleksei.uzin@gmail.com>
2026-04-18 08:34:44 +0300

    chore: update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
-import airbnb from "eslint-config-airbnb-base";
-import prettier from "eslint-config-prettier";
-import { defineConfig } from "@eslint/config-helpers";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  airbnb,
-  prettier,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+    },
+  },
 ]);
